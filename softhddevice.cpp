8d624224e932caaf6964680b1f9d4d33400e0caf
Johns <johns98@gmx.net>
2012-01-09 22:10:42 +0100

    Version 0.3.0 released.

--- softhddevice.cpp
+++ softhddevice.cpp
@@ -42,7 +42,7 @@ extern "C"
 
 //////////////////////////////////////////////////////////////////////////////
 
-static const char *const VERSION = "0.2.0";
+static const char *const VERSION = "0.3.0";
 static const char *const DESCRIPTION =
 trNOOP("A software and GPU emulated HD device");
 
