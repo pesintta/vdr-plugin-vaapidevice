9a2b6f860a3e10d97b8995abdda69d564ecf8661
Johns <johns98@gmx.net>
2012-11-15 22:30:55 +0100

    Version 0.5.2 released.

--- softhddevice.cpp
+++ softhddevice.cpp
@@ -50,7 +50,7 @@ extern "C"
     /// vdr-plugin version number.
     /// Makefile extracts the version number for generating the file name
     /// for the distribution archive.
-static const char *const VERSION = "0.5.2rc1"
+static const char *const VERSION = "0.5.2"
 #ifdef GIT_REV
     "-GIT" GIT_REV
 #endif
