2ef3025e3d31ef01f96945f7a9a1d687dd68f7b4
Antti Seppälä <a.seppala@gmail.com>
2014-09-14 12:19:10 +0300

    Lock cleanup call to protect it from video postprocessing queueing
    This might hopefully fix running into the abort() assertation with surface queue
    
    Also adding some debug logging before abort() call.

--- video.c
+++ video.c
@@ -2118,6 +2118,8 @@ static void VaapiCleanup(VaapiDecoder * decoder)
     VASurfaceID surface;
     unsigned int i;
 
+    pthread_mutex_lock(&VideoMutex);
+
     // flush output queue, only 1-2 frames buffered, no big loss
     while ((filled = atomic_read(&decoder->SurfacesFilled))) {
 	decoder->SurfaceRead = (decoder->SurfaceRead + 1) % VIDEO_SURFACES_MAX;
@@ -2137,6 +2139,8 @@ static void VaapiCleanup(VaapiDecoder * decoder)
 
 #ifdef DEBUG
     if (decoder->SurfaceRead != decoder->SurfaceWrite) {
+        Error("Surface queue mismatch. SurfaceRead = %d, SurfaceWrite = %d, SurfacesFilled = %d\n",
+              decoder->SurfaceRead, decoder->SurfaceWrite, atomic_read(&decoder->SurfacesFilled));
 	abort();
     }
 #endif
@@ -2235,6 +2239,7 @@ static void VaapiCleanup(VaapiDecoder * decoder)
     decoder->Closing = 0;
     decoder->PTS = AV_NOPTS_VALUE;
     VideoDeltaPTS = 0;
+    pthread_mutex_unlock(&VideoMutex);
 }
 
 ///
@@ -4273,6 +4278,7 @@ static void VaapiQueueSurface(VaapiDecoder * decoder, VASurfaceID surface,
     }
 #endif
 
+    pthread_mutex_lock(&VideoMutex);
     /* Queue new surface and run postprocessing filters */
     VaapiQueueSurfaceNew(decoder, surface);
     postprocessed = VaapiDeinterlaceSurface(decoder, decoder->TopFieldFirst ? 1 : 0);
@@ -4303,6 +4309,7 @@ static void VaapiQueueSurface(VaapiDecoder * decoder, VASurfaceID surface,
         decoder->SurfaceField = decoder->TopFieldFirst ? 1 : 0;
         atomic_inc(&decoder->SurfacesFilled);
     }
+    pthread_mutex_unlock(&VideoMutex);
 
     Debug(4, "video/vaapi: yy video surface %#010x ready\n", surface);
 }
