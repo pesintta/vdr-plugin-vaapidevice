f45285a0297afa1af10f58d9f929aa96617e8f9d
Antti Seppälä <a.seppala@gmail.com>
2014-10-11 12:59:12 +0300

    Revert "Sync surface after postprocessing"
    This reverts commit 21508ef740b786f159ad4064b7a23a3a7c3ca25a.
    
    The synchronization here is unnecessary. It should be done right before rendering the surface to screen
    (vaPutSurface) and before postprocessing surfaces are referenced.

--- video.c
+++ video.c
@@ -4146,7 +4146,6 @@ static VASurfaceID* VaapiDeinterlaceSurface(VaapiDecoder * decoder, int top_fiel
         surface = NULL;
     }
     vaEndPicture(VaDisplay, decoder->vpp_ctx);
-    vaSyncSurface(VaDisplay, *surface);
 
     return surface;
 }
