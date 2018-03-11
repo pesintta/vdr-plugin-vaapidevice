b4f2f637b09664e95260f49dca8de086ffeaacbe
Rolf Ahrenberg <Rolf.Ahrenberg@sci.fi>
2018-03-11 12:19:30 +0200

    Add a new "DBUG" SVDRP command.

--- generate_system_report.sh
+++ generate_system_report.sh
@@ -13,7 +13,9 @@ check_component () {
 }
 
 echo "#### VAAPIDEVICE SYSTEM INFORMATION REPORT"
-check_component "inxi" "-CGASMNzx -! 31"
+check_component "inxi" "-CGASMzx -! 31"
 check_component "vainfo" ""
-check_component "ffmpeg" "-version"
+check_component "ffmpeg" "-version" | sed '/^configuration:/d'
+check_component "gcc" "-dumpversion"
+check_component "svdrpsend" "plug vaapidevice dbug" | sed 's/^\(22[0-1]\) [a-zA-Z0-9]* \(.*\)$/\1 <filter> \2/'
 echo "##### INCLUDE THIS REPORT INTO YOUR GITHUB ISSUE"
